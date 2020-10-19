0b919ab1468f5ba43f8dccf5e469592fabc382ba
izaiah365 <izaiah.rodriguez@patelhs.org>
2020-10-19 14:00:00 +0000

    writing if and else

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import javax.lang.model.util.ElementScanner6;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int age = (17);
+    int myBirthday = (september: 17);
+    int todaysDate = 0921;
+    
+    if (age == 17);
+    {
+    System.out.println(I am + myAge + years old.);
+    }
+    else
+    {
+    System.out.println(I am still  + myAge + years old.); 
   }
+ }
 }
